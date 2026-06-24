b727ca78783277d5d66f3a965e5fa32137954218
halotesting16 <atype20@gmail.com>
2026-06-25 02:10:35 +0700

    Add Home component for Mangaheya website

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Mangaheya - Anime & Manga Hub</h1>
+
+      <p>Website rekomendasi anime dan manga terbaru 2026</p>
+
+      <h2>Menu Artikel</h2>
+
+      <ul>
+        <li><a href="/anime-terbaik-2026">Anime Terbaik 2026</a></li>
+        <li><a href="/anime-isekai-terbaik">Anime Isekai Terbaik</a></li>
+        <li><a href="/anime-action-terbaik">Anime Action Terbaik</a></li>
+        <li><a href="/anime-romance-sedih">Anime Romance Sedih</a></li>
+        <li><a href="/manga-terbaru">Manga Terbaru</a></li>
+      </ul>
+
+      <hr />
+
+      <p>
+        Baca manga di{" "}
+        <a href="https://mangaheya.net" target="_blank">
+          Mangaheya
+        </a>
+      </p>
+    </div>
+  );
+}
