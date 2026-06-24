668333e374ed05e07b76b3dcc2e76f42c117993c
halotesting16 <atype20@gmail.com>
2026-06-25 02:11:27 +0700

    Add Isekai anime list page

---
+++ pages/anime-isekai-terbaik.js
@@ -0,0 +1,16 @@
+export default function Isekai() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Anime Isekai Terbaik MC Overpower</h1>
+
+      <ul>
+        <li>Re:Zero</li>
+        <li>Mushoku Tensei</li>
+        <li>Overlord</li>
+        <li>Slime Isekai</li>
+      </ul>
+
+      <a href="https://mangaheya.net">Mangaheya</a>
+    </div>
+  );
+}
