37456a0c44305d787ac51ba8d537938f0a09e82b
halotesting16 <atype20@gmail.com>
2026-06-25 02:12:15 +0700

    Create manga-terbaru.js

---
+++ pages/manga-terbaru.js
@@ -0,0 +1,16 @@
+export default function Manga() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Manga Terbaru 2026</h1>
+
+      <ul>
+        <li>One Piece Latest</li>
+        <li>Jujutsu Kaisen Manga</li>
+        <li>Black Clover Manga</li>
+        <li>Solo Leveling Manga</li>
+      </ul>
+
+      <a href="https://mangaheya.net">Mangaheya</a>
+    </div>
+  );
+}
