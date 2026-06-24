400f709946cf28a36474a1e1c930b35b4b05d5eb
halotesting16 <atype20@gmail.com>
2026-06-25 02:12:01 +0700

    Create anime-romance-sedih.js

---
+++ pages/anime-romance-sedih.js
@@ -0,0 +1,16 @@
+export default function Romance() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Anime Romance Sedih Bikin Nangis</h1>
+
+      <ul>
+        <li>Your Name</li>
+        <li>Clannad</li>
+        <li>A Silent Voice</li>
+        <li>I Want to Eat Your Pancreas</li>
+      </ul>
+
+      <a href="https://mangaheya.net">Mangaheya</a>
+    </div>
+  );
+}
