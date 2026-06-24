a5d8a3fa974ac92a3232d7fd449380a1c7f6adbb
halotesting16 <atype20@gmail.com>
2026-06-25 02:11:02 +0700

    Add Anime2026 component with recommended titles

---
+++ pages/anime-terbaik-2026.js
@@ -0,0 +1,18 @@
+export default function Anime2026() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Anime Terbaik 2026 Wajib Ditonton</h1>
+
+      <ul>
+        <li>Jujutsu Kaisen</li>
+        <li>Demon Slayer</li>
+        <li>Solo Leveling</li>
+        <li>Black Clover</li>
+      </ul>
+
+      <p>Rekomendasi anime paling populer tahun ini.</p>
+
+      <a href="https://mangaheya.net">Mangaheya</a>
+    </div>
+  );
+}
