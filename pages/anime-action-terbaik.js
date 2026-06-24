c32641d15d63ea5ea9c96566d1061c718a73435d
halotesting16 <atype20@gmail.com>
2026-06-25 02:11:42 +0700

    Create anime-action-terbaik.js

---
+++ pages/anime-action-terbaik.js
@@ -0,0 +1,16 @@
+export default function Action() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Anime Action Terbaik Sepanjang Masa</h1>
+
+      <ul>
+        <li>Attack on Titan</li>
+        <li>Jujutsu Kaisen</li>
+        <li>Bleach</li>
+        <li>Naruto</li>
+      </ul>
+
+      <a href="https://mangaheya.net">Mangaheya</a>
+    </div>
+  );
+}
